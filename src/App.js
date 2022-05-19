13aa947c2ad5b20b3c8ab8bdf772a74fddc11bad
beep13 <97983228+beep13@users.noreply.github.com>
2022-05-19 12:34:22 -0400

    working front end done

--- src/App.js
+++ src/App.js
@@ -3,52 +3,25 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import SignIn from './components/SignIn/SignIn'
+import Register from './components/Register/Register'
 import ImgDisp from './components/ImgDisp/ImgDisp'
+import Clarifai from 'clarifai'
 import './App.css';
 
-/*const app = new Clarifai.App({
+const app = new Clarifai.App({
  apiKey: '2e2b3e8adf1d407e98e0d92f4d8d0d31'
-});*/
+});
 
-    const USER_ID = '77fp2fth65xn'
-    const PAT = '3a466543258d4ba7b4b9d950926a894e';
-    const APP_ID = '1e516835bad84453a7c27434a24524f8';
-    const MODEL_ID = 'face-detection';
-    const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
-    // Change this to whatever image URL you want to process
-    let IMAGE_URL = 'https://static.generated.photos/vue-static/face-generator/landing/wall/12.jpg';
-
-const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": USER_ID,
-            "app_id": APP_ID
-        },
-        "inputs": [
-            {
-                "data": {
-                    "image": {
-                        "url": IMAGE_URL
-                    }
-                }
-            }
-        ]
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    };
-
-    class App extends Component {
+class App extends Component {
       constructor() {
         super();
         this.state = {
           input: '',
           imgUrl: '',
+          box: {},
+          route: 'SignIn',
+          isSignedIn: false
         }
       }
 
@@ -56,29 +29,68 @@ onInputChange = (event) => {
   this.setState({input: event.target.value})
 }
 
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
 onButtonSubmit = () => {
-  this.setState({imgUrl: this.state.input})
-  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+  this.setState({imgUrl: this.state.input});
+  app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+}
+
+onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState({isSignedIn: false})
+  } else if (route === 'home'){
+    this.setState({isSignedIn: true})
+  }
+  this.setState({route: route});
 }
 
   render() {
+  const { isSignedIn, imgUrl, route, box } = this.state;
   return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={this.onInputChange}
-        onButtonSubmit={this.onButtonSubmit}
-      />
-      <ImgDisp imageUrl={this.state.imgUrl} />
+    <div className="App" >
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      {route === 'home'
+      ? <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}/>
+          <ImgDisp
+            box={box}
+            imgUrl={imgUrl}/>
+        </div>
+      : (
+        route === 'SignIn'
+        ? <SignIn onRouteChange={this.onRouteChange}/>
+        : <Register onRouteChange={this.onRouteChange}/>
+      )
+    }
     </div>
   );
 }
 }
-
 
 export default App;
--- src/components/ImgDisp/ImgDisp.js
+++ src/components/ImgDisp/ImgDisp.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../../App.css';
+import './ImgDisp.css';
 
-const ImgDisp = ({ imgUrl }) => {
+const ImgDisp = ({ imgUrl, box }) => {
   return (
-    <div>
-      <img alt='your submission' src={imgUrl} />
+    <div className='center ma'>
+      <div className='center absolute mt2' >
+        <img id='inputimage' alt='' src={imgUrl} width='500px' height='auto'/>
+        <div
+          className='bounding-box'
+          style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
+        </div>
+      </div>
     </div>
   )
 }
